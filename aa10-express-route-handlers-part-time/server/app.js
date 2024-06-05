f2bf1bb0e2861d9269e57a08c5439dc9be6a3695
Pristine Shin <157057129+pristine-shin@users.noreply.github.com>
2024-06-05 14:49:53 -0700

    Add files via upload

--- aa10-express-route-handlers-part-time/server/app.js
+++ aa10-express-route-handlers-part-time/server/app.js
@@ -24,30 +24,7 @@ const {
 const express = require('express');
 const app = express();
 
-// Your code here
-
-//Basic Phase 1: Handle JSON In Request
-  //Parse the JSON body of a request
-  app.use(express.json());
-
-  //Test a JSON request
-  app.use((req, res, next) => {
-    console.log('Request Body:', req.body);
-    next();
-  });
-
-//Basic Phase 2: Create Route Handlers
-app.get('/artists', (req, res) => {
-  res.send(getAllArtists())
-})
-
-app.post('/artists', (req, res) => {
-  const newArtist = addArtist(req.body);
-
-  res.status(201);
-  res.send(newArtist)
-})
-
+// Your code here 
 
 // DO NOT MODIFY
 if (require.main === module) {
