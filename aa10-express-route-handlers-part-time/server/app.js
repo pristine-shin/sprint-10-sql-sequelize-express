b9ea0ef54fe6b5a5cc556fc9f7799b87f4a1afa1
Pristine Shin <prisvsyo@gmail.com>
2024-06-05 13:26:48 -0700

    completed basic phases for express route handlers hw

--- aa10-express-route-handlers-part-time/server/app.js
+++ aa10-express-route-handlers-part-time/server/app.js
@@ -24,7 +24,30 @@ const {
 const express = require('express');
 const app = express();
 
-// Your code here 
+// Your code here
+
+//Basic Phase 1: Handle JSON In Request
+  //Parse the JSON body of a request
+  app.use(express.json());
+
+  //Test a JSON request
+  app.use((req, res, next) => {
+    console.log('Request Body:', req.body);
+    next();
+  });
+
+//Basic Phase 2: Create Route Handlers
+app.get('/artists', (req, res) => {
+  res.send(getAllArtists())
+})
+
+app.post('/artists', (req, res) => {
+  const newArtist = addArtist(req.body);
+
+  res.status(201);
+  res.send(newArtist)
+})
+
 
 // DO NOT MODIFY
 if (require.main === module) {
