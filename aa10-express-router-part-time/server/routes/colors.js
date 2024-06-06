90ad5a7f11bfc49b013ad7e7ba4e03159a4d1aee
Pristine Shin <prisvsyo@gmail.com>
2024-06-06 16:31:36 -0700

    completed bonus

--- aa10-express-router-part-time/server/routes/colors.js
+++ aa10-express-router-part-time/server/routes/colors.js
@@ -10,4 +10,12 @@ router.get("/:name", (req, res) => {
   res.json("GET /colors/:name");
 });
 
+router.post("/:name/css-styles", (req, res) => {
+  res.json("POST /colors/:name/css-styles");
+});
+
+router.delete("/:name/css-styles/:style", (req, res) => {
+  res.json("DELETE /colors/:name/css-styles/:style");
+});
+
 module.exports = router;
