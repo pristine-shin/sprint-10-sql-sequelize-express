6cdd82fe6720cbc659f1d66a335207e382152814
Pristine Shin <prisvsyo@gmail.com>
2024-06-12 16:04:31 -0700

    completed delete on cascade

--- aa10-sql-delete-on-cascade-part-time/sql-practice/music.sql
+++ aa10-sql-delete-on-cascade-part-time/sql-practice/music.sql
@@ -1,6 +1,7 @@
 PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS musicians;
 DROP TABLE IF EXISTS bands;
+
 CREATE TABLE bands (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(100)
@@ -11,14 +12,16 @@ CREATE TABLE musicians (
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100),
   band_id INTEGER,
-  FOREIGN KEY (band_id) REFERENCES bands(id)
+  FOREIGN KEY (band_id) REFERENCES bands(id) ON DELETE CASCADE
 );
+
 INSERT INTO bands
 VALUES (1, 'The Falling Box'),
   (2, 'America The Piano'),
   (3, 'Loved Autumn'),
   (4, 'Playin Sound'),
   (5, 'The King River');
+
 INSERT INTO musicians
 VALUES (1, 'Adam', 'Appleby', 1),
   (2, 'Anton', 'Martinovic', 1),
@@ -30,3 +33,6 @@ VALUES (1, 'Adam', 'Appleby', 1),
   (8, 'Camila', 'Nenci', 4),
   (9, 'Rosemarie', 'Affini', 5),
   (10, 'Victoria', 'Cremonesi', 5);
+
+--1
+DELETE FROM bands WHERE id = 1;
