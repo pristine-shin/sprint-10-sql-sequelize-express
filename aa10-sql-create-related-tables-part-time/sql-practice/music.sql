d513ecd2cf9f0937e90c267783a12ed6aa33faec
Pristine Shin <prisvsyo@gmail.com>
2024-06-12 15:55:37 -0700

    completed sql create related tables

--- aa10-sql-create-related-tables-part-time/sql-practice/music.sql
+++ aa10-sql-create-related-tables-part-time/sql-practice/music.sql
@@ -3,6 +3,8 @@ PRAGMA foreign_keys = 1;
 DROP TABLE IF EXISTS instruments;
 DROP TABLE IF EXISTS musicians;
 DROP TABLE IF EXISTS bands;
+DROP TABLE IF EXISTS musician_instrument;
+
 
 CREATE TABLE bands (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,9 +13,19 @@ CREATE TABLE bands (
 CREATE TABLE musicians (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100)
+  last_name VARCHAR(100),
+  mucisian_id INTEGER,
+  FOREIGN KEY (mucisian_id) REFERENCES mucisians(id)
 );
+
 CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   type VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE musician_instrument (
+  musician_id INTEGER,
+  instrument_id INTEGER,
+  FOREIGN KEY (musician_id) REFERENCES musicians(id)
+  FOREIGN KEY (instrument_id) REFERENCES instruments(id)
 );
