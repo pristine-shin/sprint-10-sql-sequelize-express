0f1ca014cd0150f49cab7af0bb173f6653d4314a
Pristine Shin <prisvsyo@gmail.com>
2024-06-12 17:40:06 -0700

    completed join many to many sql

--- aa10-sql-join-many-to-many-part-time/sql-practice/step-1.sql
+++ aa10-sql-join-many-to-many-part-time/sql-practice/step-1.sql
@@ -2,4 +2,6 @@
 -- Connecting both sides of a many-to-many relationship involves JOINing the
 -- join table on to one side of the relationship, then JOINing the other side
 -- on to the join table.
--- Your code here 
+-- Your code here
+SELECT musicians.first_name, instruments.type FROM musician_instruments
+  JOIN musicians, instruments ON (musician_instruments.musician_id = musicians.id AND musician_instruments.instrument_id = instruments.id);
--- aa10-sql-join-many-to-many-part-time/sql-practice/step-2.sql
+++ aa10-sql-join-many-to-many-part-time/sql-practice/step-2.sql
@@ -1,4 +1,6 @@
 -- Step 2
 -- Just like with one-to-many relationships, you can filter one table by any
 -- associated data on a conected table.
--- Your code here 
+-- Your code here
+SELECT musicians.first_name, musicians.last_name FROM musician_instruments
+  JOIN musicians, instruments ON (musician_instruments.musician_id = musicians.id AND musician_instruments.instrument_id = instruments.id AND instruments.type = 'piano');
